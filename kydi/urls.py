2ee35866b380dd849b19477dc17a36c3d300023d
Amos-e <amosemmanelz@gmail.com>
2025-03-03 15:31:41 +0300

    Favicon

--- kydi/urls.py
+++ kydi/urls.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
+from django.urls import path, include, re_path
 import accounts
 from rest_framework.authtoken import views
 from accounts.serializers import CustomAuthToken
+
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,6 +19,7 @@ urlpatterns = [
     path('register/complete', accounts.views.django_registration_complete, name='django_registration_complete'),
     path('activate/complete', accounts.views.django_activation_complete, name='django_registration_activation_complete'),
     path('api-token-auth/', CustomAuthToken.as_view()),
+    re_path(r'^favicon\.ico$', favicon_view),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
